90a31f9c4c8878a99a4e9a5a018d0cc731cd0d96
Nat Zimmermann <nat@razorcreations.com>
2018-03-22 19:15:27 +0000

    yolo inline

--- templates/index.php
+++ templates/index.php
@@ -5,7 +5,30 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP-CS-Fixer Playground</title>
-    <link rel="stylesheet" href="/style.css">
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        .container {
+            display: flex;
+            width: 100%;
+        }
+
+        .container > div {
+            flex-grow: 1;
+            flex-basis: 0;
+        }
+
+        #code {
+            font-family: monospace;
+            width: 100%;
+        }
+
+        #form {
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
