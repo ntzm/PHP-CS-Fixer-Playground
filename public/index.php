8bb9b3438340d19cba1f796ff4a75e0e2d562f31
Nat Zimmermann <nat@razorcreations.com>
2018-03-22 19:03:17 +0000

    Extract CSS

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if ($_SERVER['REQUEST_URI'] !== '/') {
+	return false;
+}
+
 use PhpCsFixerPlayground\Fixer;
 
 if (isset($_GET['code']) && is_string($_GET['code'])) {
@@ -26,30 +30,7 @@ if (isset($_GET['code']) && is_string($_GET['code'])) {
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>PHP-CS-Fixer Playground</title>
-	<style>
-		* {
-			box-sizing: border-box;
-		}
-
-		.container {
-			display: flex;
-			width: 100%;
-		}
-
-		.container > div {
-			flex-grow: 1;
-			flex-basis: 0;
-		}
-
-		#code {
-			font-family: monospace;
-			width: 100%;
-		}
-
-		#form {
-			width: 100%;
-		}
-	</style>
+	<link rel="stylesheet" href="/style.css">
 </head>
 <body>
 	<div class="container">
