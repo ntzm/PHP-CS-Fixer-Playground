93b08fadc525e35fcaa3d7e4a0656df0aee25db5
Nat Zimmermann <nat@razorcreations.com>
2018-03-24 17:50:20 +0000

    Convert templates to twig

--- src/functions.php
+++ src/functions.php
@@ -2,28 +2,35 @@
 
 namespace PhpCsFixerPlayground;
 
+use PhpCsFixer\Console\Application;
 use PhpCsFixer\FixerFactory;
-
-function escape(string $string): string
-{
-    return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
-}
-
-function format(string $string): string
-{
-    return preg_replace('/`(.+?)`/', '<code>$1</code>', $string);
-}
+use Twig_Environment;
+use Twig_Filter;
+use Twig_Loader_Filesystem;
+use Twig_Test;
 
 function view(string $code, array $fixers, string $result): string
 {
+    $loader = new Twig_Loader_Filesystem(__DIR__.'/../templates');
+    $twig = new Twig_Environment($loader);
+
+    $twig->addTest(new Twig_Test('instanceof', function (object $instance, string $class): bool {
+        return $instance instanceof $class;
+    }));
+
+    $twig->addFilter(new Twig_Filter('format', function (string $string): string {
+        return preg_replace('/`(.+?)`/', '<code>$1</code>', $string);
+    }, ['pre_escape' => 'html', 'is_safe' => ['html']]));
+
+    $phpCsFixerVersion = Application::VERSION;
+
     $availableFixers = FixerFactory::create()
         ->registerBuiltInFixers()
         ->getFixers()
     ;
 
-    ob_start();
-
-    require __DIR__.'/../templates/index.php';
-
-    return ob_get_clean();
+    return $twig->render(
+        'index.twig',
+        compact('code', 'fixers', 'result', 'availableFixers', 'phpCsFixerVersion')
+    );
 }
