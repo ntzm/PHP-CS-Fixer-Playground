0f39043e33d86ff5f60f1ee060c054b547ebe6cd
Nat Zimmermann <nat@razorcreations.com>
2018-03-25 22:29:45 +0100

    Actually run php-cs-fixer

--- src/RunRepositoryInterface.php
+++ src/RunRepositoryInterface.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PhpCsFixerPlayground;
 
 interface RunRepositoryInterface
 {
     public function getByHash(string $hash): Run;
+
     public function save(Run $run): Run;
 }
