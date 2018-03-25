198237f721eae00f934b38cecca3da644bca0886
Nat Zimmermann <nat@razorcreations.com>
2018-03-25 16:20:51 +0100

    Add container

--- html/index.php
+++ html/index.php
@@ -6,6 +6,9 @@ use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
 use Hashids\Hashids;
+use Hashids\HashidsInterface;
+use League\Container\Container;
+use League\Container\ReflectionContainer;
 use PhpCsFixerPlayground\ConnectionResolver;
 use PhpCsFixerPlayground\Handler\CreateRunHandler;
 use PhpCsFixerPlayground\Handler\GetRunHandler;
@@ -13,26 +16,44 @@ use PhpCsFixerPlayground\Handler\HandlerInterface;
 use PhpCsFixerPlayground\Handler\IndexHandler;
 use PhpCsFixerPlayground\RunNotFoundException;
 use PhpCsFixerPlayground\RunRepository;
+use PhpCsFixerPlayground\RunRepositoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 require __DIR__.'/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
-$connection = (new ConnectionResolver())->resolve();
-$hashids = new Hashids(getenv('HASHIDS_SECRET'), 10);
 
-$dispatcher = simpleDispatcher(function (RouteCollector $r) use ($connection, $hashids) {
-    $r->get('/', function() {
-        return new IndexHandler();
+$container = new Container();
+$container->delegate(new ReflectionContainer());
+
+$container->add(
+    HashidsInterface::class,
+    new Hashids(getenv('HASHIDS_SECRET'), 10)
+);
+
+$container->add(
+    PDO::class,
+    (new ConnectionResolver())->resolve()
+);
+
+$container
+    ->add(RunRepositoryInterface::class, RunRepository::class)
+    ->withArgument(PDO::class)
+    ->withArgument(HashidsInterface::class)
+;
+
+$dispatcher = simpleDispatcher(function (RouteCollector $r) use ($container): void {
+    $r->get('/', function() use ($container): IndexHandler {
+        return $container->get(IndexHandler::class);
     });
 
-    $r->post('/', function () use ($connection, $hashids) {
-        return new CreateRunHandler(new RunRepository($connection, $hashids));
+    $r->post('/', function () use ($container): CreateRunHandler {
+        return $container->get(CreateRunHandler::class);
     });
 
-    $r->get('/{hash:[a-zA-Z0-9]+}', function () use ($connection, $hashids) {
-        return new GetRunHandler(new RunRepository($connection, $hashids));
+    $r->get('/{hash:[a-zA-Z0-9]+}', function () use ($container): GetRunHandler {
+        return $container->get(GetRunHandler::class);
     });
 });
 
