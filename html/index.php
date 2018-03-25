66a59ee3498c7527a1bbdd68afff7ed3e851ab48
Nat Zimmermann <nat@razorcreations.com>
2018-03-25 17:16:38 +0100

    Extract RouteHandler

--- html/index.php
+++ html/index.php
@@ -2,54 +2,28 @@
 
 declare(strict_types=1);
 
-use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
 use PhpCsFixerPlayground\Container;
 use PhpCsFixerPlayground\Handler\CreateRunHandler;
 use PhpCsFixerPlayground\Handler\GetRunHandler;
-use PhpCsFixerPlayground\Handler\HandlerInterface;
 use PhpCsFixerPlayground\Handler\IndexHandler;
-use PhpCsFixerPlayground\RunNotFoundException;
+use PhpCsFixerPlayground\RouteHandler;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 require __DIR__.'/../vendor/autoload.php';
 
 $container = new Container();
 
+$dispatcher = simpleDispatcher(function (RouteCollector $r) use ($container): void {
+    $r->get('/', $container->get(IndexHandler::class));
+    $r->post('/', $container->get(CreateRunHandler::class));
+    $r->get('/{hash:[a-zA-Z0-9]+}', $container->get(GetRunHandler::class));
+});
+
 /** @var Request $request */
 $request = $container->get(Request::class);
 
-$dispatcher = simpleDispatcher(function (RouteCollector $r) use ($container): void {
-    $r->get('/', function() use ($container): IndexHandler {
-        return $container->get(IndexHandler::class);
-    });
-
-    $r->post('/', function () use ($container): CreateRunHandler {
-        return $container->get(CreateRunHandler::class);
-    });
-
-    $r->get('/{hash:[a-zA-Z0-9]+}', function () use ($container): GetRunHandler {
-        return $container->get(GetRunHandler::class);
-    });
-});
-
-$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPathInfo());
-
-switch ($routeInfo[0]) {
-    case Dispatcher::NOT_FOUND:
-        Response::create('Not Found', Response::HTTP_NOT_FOUND)->send();
-    case Dispatcher::METHOD_NOT_ALLOWED:
-        Response::create('Method Not Allowed', Response::HTTP_METHOD_NOT_ALLOWED)->send();
-    case Dispatcher::FOUND:
-        /** @var HandlerInterface $handler */
-        $handler = $routeInfo[1]();
-        $vars = $routeInfo[2];
-
-        try {
-            $handler($vars)->send();
-        } catch (RunNotFoundException $e) {
-            Response::create('Not Found', Response::HTTP_NOT_FOUND)->send();
-        }
-}
+(new RouteHandler())->handle(
+    $dispatcher->dispatch($request->getMethod(), $request->getPathInfo())
+)->send();
---
+++ src/RouteHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PhpCsFixerPlayground;
+
+use FastRoute\Dispatcher;
+use Symfony\Component\HttpFoundation\Response;
+
+final class RouteHandler
+{
+    public function handle(array $routeInfo): Response
+    {
+        $status = $routeInfo[0];
+
+        if ($status === Dispatcher::NOT_FOUND) {
+            return $this->handleNotFound();
+        }
+
+        if ($status === Dispatcher::METHOD_NOT_ALLOWED) {
+            return $this->handleMethodNotAllowed();
+        }
+
+        /** @var Handler\HandlerInterface $handler */
+        $handler = $routeInfo[1];
+
+        /** @var array $vars */
+        $vars = $routeInfo[2];
+
+        try {
+            return $handler($vars);
+        } catch (RunNotFoundException $e) {
+            return $this->handleNotFound();
+        }
+    }
+
+    private function handleNotFound(): Response
+    {
+        return Response::create('Not Found', Response::HTTP_NOT_FOUND);
+    }
+
+    private function handleMethodNotAllowed(): Response
+    {
+        return Response::create('Method Not Allowed', Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+}
