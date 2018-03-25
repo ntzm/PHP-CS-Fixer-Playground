c5cafd2ee0854fd2fa1e103dafc3ce8a2c88a78e
Nat Zimmermann <nat@razorcreations.com>
2018-03-25 22:37:28 +0100

    Add basic phpunit

---
+++ tests/RunTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PhpCsFixerPlayground\Tests;
+
+use PhpCsFixer\Tests\TestCase;
+use PhpCsFixerPlayground\Run;
+
+final class RunTest extends TestCase
+{
+    public function testGetCode(): void
+    {
+        $run = new Run('<?php echo "hi";', []);
+
+        $this->assertSame('<?php echo "hi";', $run->getCode());
+    }
+
+    public function testGetRules(): void
+    {
+        $run = new Run('<?php echo "hi";', ['single_quote']);
+
+        $this->assertSame(['single_quote'], $run->getRules());
+    }
+
+    public function testGetHashUnsaved(): void
+    {
+        $run = new Run('<?php echo "hi";', []);
+
+        $this->assertNull($run->getHash());
+    }
+
+    public function testGetHashSaved(): void
+    {
+        $run = new Run('<?php echo "hi";', [], 'foobar');
+
+        $this->assertSame('foobar', $run->getHash());
+    }
+}
