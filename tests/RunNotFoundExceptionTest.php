aef9daff890f908b5a9a76879f0a7eb6aa4fa843
Nat Zimmermann <nat@razorcreations.com>
2018-03-26 12:35:06 +0100

    Add RunNotFoundException test

---
+++ tests/RunNotFoundExceptionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCsFixerPlayground\Tests;
+
+use PhpCsFixerPlayground\RunNotFoundException;
+use PHPUnit\Framework\TestCase;
+
+final class RunNotFoundExceptionTest extends TestCase
+{
+	public function testFromHash(): void
+	{
+		$this->expectException(RunNotFoundException::class);
+		$this->expectExceptionMessage('Cannot find run with hash foo');
+
+		throw RunNotFoundException::fromHash('foo');
+	}
+}
