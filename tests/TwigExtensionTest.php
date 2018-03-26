9bc1d971274715cccd8463cca8206678d84ebd3d
Nat Zimmermann <nat@razorcreations.com>
2018-03-27 00:47:31 +0100

    Test twig extensions

---
+++ tests/TwigExtensionTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCsFixerPlayground\Tests;
+
+use DateTime;
+use PhpCsFixerPlayground\TwigExtension;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+/**
+ * @covers \PhpCsFixerPlayground\TwigExtension
+ */
+final class TwigExtensionTest extends TestCase
+{
+    public function testInstanceOfTest(): void
+    {
+        $extension = new TwigExtension();
+
+        $instance = new stdClass();
+
+        $this->assertTrue($extension->instanceofTest($instance, stdClass::class));
+        $this->assertFalse($extension->instanceofTest($instance, DateTime::class));
+    }
+
+    /**
+     * @param string $expected
+     * @param string $input
+     *
+     * @dataProvider provideFormatFilterCases
+     */
+    public function testFormatFilter(string $expected, string $input): void
+    {
+        $extension = new TwigExtension();
+
+        $this->assertSame($expected, $extension->formatFilter($input));
+    }
+
+    public function provideFormatFilterCases(): array
+    {
+        return [
+            [
+                'foo',
+                'foo',
+            ],
+            [
+                '<code>foo</code>',
+                '`foo`',
+            ],
+            [
+                '<script>alert(1);</script><code>foo</code>',
+                '<script>alert(1);</script>`foo`',
+            ],
+            [
+                '``',
+                '``',
+            ],
+        ];
+    }
+
+    /**
+     * @param string   $expected
+     * @param string[] $rules
+     *
+     * @dataProvider provideLinkRulesFilterCases
+     */
+    public function testLinkRulesFilter(string $expected, array $rules): void
+    {
+        $extension = new TwigExtension();
+
+        $this->assertSame($expected, $extension->linkRulesFilter($rules));
+    }
+
+    public function provideLinkRulesFilterCases(): array
+    {
+        return [
+            [
+                '',
+                [],
+            ],
+            [
+                '<a href="#foo"><code>foo</code></a>',
+                ['foo'],
+            ],
+            [
+                '<a href="#foo"><code>foo</code></a>, <a href="#bar"><code>bar</code></a>, <a href="#baz"><code>baz</code></a>',
+                ['foo', 'bar', 'baz'],
+            ],
+        ];
+    }
+}
