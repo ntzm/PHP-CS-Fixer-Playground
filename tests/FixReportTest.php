a700fc5dfbaf748a2ab693c3a12e2c6d22663026
Nat Zimmermann <nat@razorcreations.com>
2018-03-29 19:29:23 +0100

    Add tests for FixReport

---
+++ tests/FixReportTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCsFixerPlayground\Tests;
+
+use PhpCsFixer\Fixer\Alias\NoAliasFunctionsFixer;
+use PhpCsFixerPlayground\FixReport;
+use PHPUnit\Framework\TestCase;
+
+final class FixReportTest extends TestCase
+{
+    public function testGetResult(): void
+    {
+        $report = new FixReport('<?php echo "hi";', []);
+
+        $this->assertSame('<?php echo "hi";', $report->getResult());
+    }
+
+    public function testGetAppliedFixers(): void
+    {
+        $fixers = [new NoAliasFunctionsFixer()];
+
+        $report = new FixReport('<?php echo "hi";', $fixers);
+
+        $this->assertEquals($fixers, $report->getAppliedFixers());
+    }
+}
