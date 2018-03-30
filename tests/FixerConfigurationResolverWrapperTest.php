fc16369a3da8e14ef6c13d45629161544988304b
Nat Zimmermann <nat@razorcreations.com>
2018-03-30 16:37:50 +0100

    Add FixerConfigurationResolverWrapper tests

---
+++ tests/FixerConfigurationResolverWrapperTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCsFixerPlayground\Tests;
+
+use PhpCsFixer\FixerConfiguration\FixerConfigurationResolverInterface;
+use PhpCsFixer\FixerConfiguration\FixerOptionInterface;
+use PhpCsFixerPlayground\FixerConfigurationResolverWrapper;
+use PhpCsFixerPlayground\FixerOptionWrapper;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \PhpCsFixerPlayground\FixerConfigurationResolverWrapper
+ */
+final class FixerConfigurationResolverWrapperTest extends TestCase
+{
+    public function testGetOptions(): void
+    {
+        $resolver = $this->createMock(FixerConfigurationResolverInterface::class);
+        $resolver->method('getOptions')->willReturn([
+            $this->createMock(FixerOptionInterface::class),
+            $this->createMock(FixerOptionInterface::class),
+        ]);
+
+        $wrapper = new FixerConfigurationResolverWrapper($resolver);
+
+        $this->assertContainsOnlyInstancesOf(FixerOptionWrapper::class, $wrapper->getOptions());
+    }
+
+    public function testResolve(): void
+    {
+        $resolver = $this->createMock(FixerConfigurationResolverInterface::class);
+        $resolver->method('resolve')->willReturn(['foo' => 'bar']);
+
+        $wrapper = new FixerConfigurationResolverWrapper($resolver);
+
+        $this->assertSame(['foo' => 'bar'], $wrapper->resolve([]));
+    }
+}
