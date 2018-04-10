bf28ec3f3a21695a4987578adddb45d6c0998adc
Nat Zimmermann <nat@razorcreations.com>
2018-04-10 13:30:33 +0100

    Test version switcher

--- tests/PhpCsFixerVersion/VersionSwitcherTest.php
+++ tests/PhpCsFixerVersion/VersionSwitcherTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace PhpCsFixerPlayground\Tests\PhpCsFixerVersion;
 
+use org\bovigo\vfs\vfsStream;
+use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 use PhpCsFixerPlayground\Entity\PhpCsFixerVersion;
 use PhpCsFixerPlayground\PhpCsFixerVersion\VersionSwitcher;
@@ -26,5 +28,46 @@ final class VersionSwitcherTest extends TestCase
         $this->expectExceptionMessage('Class PhpCsFixer\Finder already autoloaded');
 
         $switcher->switchTo(new PhpCsFixerVersion('2.11.1'));
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testAutoloadsFromDirectory(): void
+    {
+        $path = vfsStream::setup('root', null, [
+            '2.11.1' => [
+                'Config.php' => '<?php namespace PhpCsFixer; class Config { public function getFormat() { return "bar"; } }',
+            ],
+        ]);
+
+        $switcher = new VersionSwitcher($path->url());
+
+        $switcher->switchTo(new PhpCsFixerVersion('2.11.1'));
+
+        $this->assertSame('bar', (new Config())->getFormat());
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testAutoloadsFromDirectoryFailsWhenDoesNotExist(): void
+    {
+        $path = vfsStream::setup('root', null, [
+            '2.11.1' => [
+                'Config.php' => '<?php namespace PhpCsFixer; class Config { public function getFormat() { return "bar"; } }',
+            ],
+        ]);
+
+        $switcher = new VersionSwitcher($path->url());
+
+        $switcher->switchTo(new PhpCsFixerVersion('2.11.1'));
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Cannot find class PhpCsFixer\Finder on PHP-CS-Fixer version 2.11.1, tried file vfs://root/2.11.1/Finder.php');
+
+        new Finder();
     }
 }
