a306f3ebb506f1ba222143430836af28cd388bd4
Nat Zimmermann <nat@razorcreations.com>
2018-04-15 16:06:56 +0100

    Add missing line ending test

--- tests/LineEndingTest.php
+++ tests/LineEndingTest.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace PhpCsFixerPlayground\Tests;
 
+use InvalidArgumentException;
 use PhpCsFixerPlayground\LineEnding;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @covers \PhpCsFixerPlayground\LineEnding
+ */
 final class LineEndingTest extends TestCase
 {
     public function testGetVisible(): void
@@ -25,5 +29,27 @@ final class LineEndingTest extends TestCase
     {
         $this->assertSame("\n", LineEnding::fromVisible('\n')->getReal());
         $this->assertSame("\r\n", LineEnding::fromVisible('\r\n')->getReal());
+    }
+
+    public function testToString(): void
+    {
+        $this->assertSame("\n", (string) new LineEnding("\n"));
+        $this->assertSame("\r\n", (string) new LineEnding("\r\n"));
+    }
+
+    public function testConstructInvalid(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid line ending foo, must be one of \n, \r\n');
+
+        new LineEnding('foo');
+    }
+
+    public function testFromVisibleInvalid(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid line ending foo, must be one of \n, \r\n');
+
+        LineEnding::fromVisible('foo');
     }
 }
